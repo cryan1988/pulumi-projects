6ac3e0c75283ab63c5c1b49f1dbe76335dbd4f04
Conal Ryan <cryan@apptio.com>
2023-10-13 16:11:35 +0100

    testing web-app config

--- web-app/__main__.py
+++ web-app/__main__.py
@@ -1,21 +1,70 @@
 """A Kubernetes Python Pulumi program"""
 
 import pulumi
-from pulumi_kubernetes.apps.v1 import Deployment, DeploymentSpecArgs
-from pulumi_kubernetes.meta.v1 import LabelSelectorArgs, ObjectMetaArgs
-from pulumi_kubernetes.core.v1 import ContainerArgs, PodSpecArgs, PodTemplateSpecArgs
+from pulumi_kubernetes.apps.v1 import Deployment
+from pulumi_kubernetes.core.v1 import Service, Namespace
+from pulumi_kubernetes.networking.v1 import Ingress
 
-app_labels = { "app": "nginx" }
+# Define the Kubernetes namespace
+namespace = Namespace('web-app')
 
-deployment = Deployment(
-    "nginx",
-    spec=DeploymentSpecArgs(
-        selector=LabelSelectorArgs(match_labels=app_labels),
-        replicas=1,
-        template=PodTemplateSpecArgs(
-            metadata=ObjectMetaArgs(labels=app_labels),
-            spec=PodSpecArgs(containers=[ContainerArgs(name="nginx", image="nginx")])
-        ),
-    ))
+# Create Deployment within the specified namespace
+deployment = Deployment('web-app-deployment',
+    metadata={
+        'namespace': namespace.metadata['name'],
+        'labels': {'app': 'web-app'},
+    },
+    spec={
+        'selector': {
+            'match_labels': {'app': 'web-app'},
+        },
+        'replicas': 2,
+        'template': {
+            'metadata': {'labels': {'app': 'web-app'}},
+            'spec': {
+                'containers': [{
+                    'name': 'web-app',
+                    'image': 'docker.apps.papt.to/test/web-app-image',
+                    'ports': [{'container_port': 8080}],
+                }],
+            },
+        },
+    })
 
-pulumi.export("name", deployment.metadata["name"])
+# Create Service
+service = Service('web-app-service',
+    metadata={
+        'labels': {'app': 'web-app'},
+        'namespace': namespace.metadata['name'],
+    },
+    spec={
+        'selector': {'app': 'web-app'},
+        'ports': [{'port': 80, 'target_port': 8080}],
+    })
+
+# Create Ingress
+#ingress = Ingress('web-app-ingress',
+#    metadata={
+#        'labels': {'app': 'web-app'},
+#        'namespace': namespace.metadata['name'],
+#    },
+#    spec={
+#        "rules": [
+#            {
+#                "http": {
+#                    "paths": [
+#                        {
+#                            "backend": {
+#                                "service": {
+#                                    "name": "test",
+#                                    "port": {"number": 80},
+#                                }
+#                            },
+#                            "path": "/testpath",
+#                            "pathType": "Prefix",
+#                        }
+#                    ]
+#                }
+#            }
+#        ]
+#    })
